c07db02d71b0a619f890b47a642f231a0a2c50be
boogameow <76601381+boogameow@users.noreply.github.com>
2021-01-01 19:28:42 -0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -23,7 +23,7 @@ local endchasetime = 6
 
 -- SURVIVOR:
 -- objective
-local computerbp = 12 -- awarded per % of gen
+local computerbp = 14 -- awarded per % of gen
 local opengatebp = 1500
 
 -- survival
