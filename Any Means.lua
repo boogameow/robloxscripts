2f2a7e2f4ece9cec66105670af0502019776a086
boogameow <76601381+boogameow@users.noreply.github.com>
2021-04-10 07:40:59 -0700

    cleaned up

--- Any Means.lua
+++ Any Means.lua
@@ -1,38 +1,37 @@
+if not game:IsLoaded() then
+    game.Loaded:Wait()
+end
+
 local rep = game:GetService("ReplicatedStorage")
-	local update = rep.RemoteEvents.PropertieUpdater
-
-local serv = game:GetService("UserInputService")
-
-local ps = game:GetService("Players")
-	local pl = ps.LocalPlayer
-	local char = pl.Character or pl.CharacterAdded:Wait()
+local update = rep:WaitForChild("RemoteEvents"):WaitForChild("PropertieUpdater")
+local cas = game:GetService("ContextActionService")
+local pl = game:GetService("Players").LocalPlayer
 
 local pallets = {}
 
-if not workspace:FindFirstChild("Pallet1") then
-	warn("Not Loaded. Execute this later.")
-else 
-	for i, v in pairs(workspace:GetChildren()) do
-		if string.find(v.Name, "Pallet") then
-			table.insert(pallets, #pallets + 1, v)
-		end
+for i, v in pairs(workspace:GetChildren()) do
+	if string.find(v.Name, "Pallet") then
+		table.insert(pallets, #pallets + 1, v)
 	end
+end
 
-	serv.InputBegan:Connect(function(inp, proc)
-		if proc then return elseif inp.KeyCode == Enum.KeyCode.F then
-			for i, v in pairs(pallets) do
-				local state = v.Panel.State
+local function action(n, st)
+	if st == Enum.UserInputState.Begin and pl.Character then
+		local pos = pl.Character.HumanoidRootPart.Position
 
-				if state.Value ~= 0 then
-					local mag = (char.HumanoidRootPart.Position - v.Body.Position).magnitude
+		for i, v in pairs(pallets) do
+			local state = v.Panel.State
 
-					if mag <= 10 then
-						update:FireServer(state, 0)
-					end
+			if state.Value ~= 0 then
+				local mag = (pos - v.Body.Position).magnitude
+
+				if mag <= 10 then
+					update:FireServer(state, 0)
 				end
 			end
 		end
-	end)
-
-	print("Loaded AMN.")
+	end
 end
+
+cas:BindAction("AMN", action, false, Enum.KeyCode.F)
+print("Loaded AMN.")
