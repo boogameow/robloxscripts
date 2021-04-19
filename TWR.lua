4ae23877628f0cc72383f2b9f8dace89b67f4e63
boogameow <76601381+boogameow@users.noreply.github.com>
2021-04-18 20:38:17 -0700

    twr hopper

---
+++ TWR.lua
@@ -0,0 +1,55 @@
+syn.queue_on_teleport(readfile("twrauto.lua"))
+rconsolename("TWR Hopper")
+
+local minwave = 10
+
+local tp = game:GetService("TeleportService")
+local rep = game:GetService("ReplicatedStorage")
+local wave = rep:WaitForChild("Game Stuff"):WaitForChild("Wave")
+local http = game:GetService("HttpService")
+
+local servers = tp:GetTeleportSetting("servers")
+
+local function url(cursor)
+    return string.format("https://games.roblox.com/v1/games/%s/servers/Public?sortOrder=Asc&limit=100&cursor=%s", game.PlaceId, cursor)
+end
+
+local function check()
+    if not servers or #servers == 0 then
+        servers = {}
+        local cursor = ""
+
+        while true do
+            local t = http:JSONDecode(game:HttpGet(url(cursor)))
+
+            for i, v in ipairs(t.data) do
+                table.insert(servers, v.id)
+           	end
+
+            if not t["nextPageCursor"] or t["nextPageCursor"] == cursor then
+                break
+            end
+
+            cursor = t["nextPageCursor"]
+        end
+    end
+end
+
+local function dotp()
+	check()
+
+	local nextserver = servers[#servers]
+    table.remove(servers, #servers)
+
+	tp:SetTeleportSetting("servers", servers)
+    tp:TeleportToPlaceInstance(game.PlaceId, nextserver)
+end
+
+if wave.Value < minwave then
+	rconsoleprint("WAVE: " .. tostring(wave.Value) .. " | NOT ELGIBILE")
+
+	tp.TeleportInitFailed:Connect(dotp)
+	dotp()
+else 
+	rconsoleprint("WAVE: " .. tostring(wave.Value) .. " | ELGIBILE")
+end
---
+++ TWRLoader.lua
@@ -0,0 +1,2 @@
+writefile("twrauto.lua", game:HttpGet("https://raw.githubusercontent.com/boogameow/scripts/master/TWR.lua"))
+loadfile("twrauto.lua")()
