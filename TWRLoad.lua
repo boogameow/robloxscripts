d68b7d1c578532da7108c37daa18ef7860d88920
boogameow <76601381+boogameow@users.noreply.github.com>
2021-04-18 20:57:08 -0700

    Update TWRLoad.lua

--- TWRLoad.lua
+++ TWRLoad.lua
@@ -49,12 +49,12 @@ local function dotp()
 end
 
 if wave.Value < minwave or wave.Value == 15 then
-    rconsoleprint("\nWAVE: " .. tostring(wave.Value) .. " | NOT ELGIBILE")
+    rconsoleprint("\nWAVE: " .. tostring(wave.Value) .. " | NOT ELIGIBILE")
 
     delay(5, function()
         tp.TeleportInitFailed:Connect(dotp)
         dotp()
     end)
 else 
-    rconsoleprint("\nWAVE: " .. tostring(wave.Value) .. " | MAP: " .. map.Value .. " | ELGIBILE")
+    rconsoleprint("\nWAVE: " .. tostring(wave.Value) .. " | MAP: " .. map.Value .. " | ELIGIBILE")
 end
